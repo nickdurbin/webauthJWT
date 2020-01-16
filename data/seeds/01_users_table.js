1f76144d5432cfb4bfae240639d8b3a2eefc2420
ndurbin <nick.durbin@gmail.com>
2020-01-16 05:57:19 -0500

    Seed data schema.

--- data/seeds/01_users_table.js
+++ data/seeds/01_users_table.js
@@ -1,13 +1,8 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users')
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {id: 1, username: 'user1', password: "password", department: "Science"},
       ]);
     });
 };
