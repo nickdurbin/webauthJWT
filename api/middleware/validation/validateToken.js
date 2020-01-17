2a804d886bed083dfa0139ef29dc756107b1be21
ndurbin <nick.durbin@gmail.com>
2020-01-17 09:00:44 -0500

    Created a validation token middleware for auth.

---
+++ api/middleware/validation/validateToken.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+
+function tokenValidation(req, res, next) {
+  const { authorization } = req.headers
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || 'A secret is a secret does.'
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: "Invalid Token!"})
+      } else {
+        req.token = decodedToken;
+
+        next()
+      }
+    })
+  } else {
+    res.status(400).json({ message: "You are not verified. Please login."})
+  }
+}
+
+module.exports = {
+  tokenValidation
+}
