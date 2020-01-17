3d8fa4fdfd45949971f11fed922741b57db20332
ndurbin <nick.durbin@gmail.com>
2020-01-17 09:39:31 -0500

    Added extra seed data to test stretch.

--- data/seeds/01_users_table.js
+++ data/seeds/01_users_table.js
@@ -3,6 +3,13 @@ exports.seed = function(knex) {
     .then(function () {
       return knex('users').insert([
         {id: 1, username: 'user1', password: "password", department: "Science"},
+        {id: 2, username: 'user2', password: "password", department: "Science"},
+        {id: 3, username: 'user3', password: "password", department: "Math"},
+        {id: 4, username: 'user4', password: "password", department: "Math"},
+        {id: 5, username: 'user5', password: "password", department: "Art"},
+        {id: 6, username: 'user6', password: "password", department: "Art"},
+        {id: 7, username: 'user7', password: "password", department: "Sports"},
+        {id: 8, username: 'user8', password: "password", department: "Sports"},
       ]);
     });
 };
