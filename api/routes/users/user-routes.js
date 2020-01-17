f20e69be0590be5e8b479196b44e3ce322a07a07
ndurbin <nick.durbin@gmail.com>
2020-01-17 09:01:34 -0500

    Added the validateToken middleware to user routes.

--- api/routes/users/user-routes.js
+++ api/routes/users/user-routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const Users = require("./user-model")
 const router = express.Router()
+const { validateToken } = require('../../middleware/validation/validateToken')
 
-router.get("/", async (req, res, next) => {
+router.get("/", validateToken, async (req, res, next) => {
   try {
     const users = await Users.find()
     
