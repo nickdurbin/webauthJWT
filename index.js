4ecc3336ee359621d62a63efe58e1d79c12b85d7
ndurbin <nick.durbin@gmail.com>
2020-01-17 08:59:46 -0500

    Fixed the port variable. Typo.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const server = require('./api/server')
 
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-server.listen(port, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}.`)
 })
