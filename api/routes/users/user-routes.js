25681416bdca19e69039f571d7d2488e250b914d
nick.durbin <nick.durbin@gmail.com>
2020-01-17 14:52:30 -0500

    Added the display for users.

--- api/routes/users/user-routes.js
+++ api/routes/users/user-routes.js
@@ -6,19 +6,12 @@ const { checkDepartment } = require('../../middleware/validation/checkDepartment
 
 router.get("/", validateToken, async (req, res, next) => {
   try {
-    console.log('decoded', req.token)
     const users = await Users.find()
-
     const { department } = req.token
-    console.log('department', department)
-    console.log(users)
-    const usersInDepartment = users.map(user => {
-      console.log(user.department, department)
-      if (user.department === department) {
-        return user
-      }
-    })
-    console.log(usersInDepartment)
+    const usersInDepartment = users.filter(user => 
+      user.department === department
+    )
+   
     res.json(usersInDepartment)
   } catch (err) {
     next(err)
