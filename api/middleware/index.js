59c04206a5c6b0646fcfe43604d70d3c00211596
nick.durbin <nick.durbin@gmail.com>
2020-01-21 21:20:13 -0500

    Added a type for morgan to short.

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 module.exports = server => {
-  server.use(morgan())
+  server.use(morgan("short"))
   server.use(helmet())
   server.use(cors())
 }
